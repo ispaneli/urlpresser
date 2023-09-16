07d39a8622ed48d8e5641e71a1960b495d2bbc27
ispaneli <ilia.bezverzhenko@mail.ru>
2023-09-16 18:14:18 +0300

    🛠 Fix mistake in configuration (port from 8000 to 8080)

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -11,7 +11,7 @@ func InitConfig() *Config {
 	config := &Config{}
 
 	flag.StringVar(&config.HTTPAddress, "a", `:8080`, "HTTP Address")
-	flag.StringVar(&config.BaseURL, "b", "http://localhost:8000/", "Base URL")
+	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080/", "Base URL")
 	flag.Parse()
 
 	return config
