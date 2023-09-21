0c952ee8fbdc94147d610b00c807e758b6179b4a
ispaneli <ilia.bezverzhenko@mail.ru>
2023-09-22 01:27:18 +0300

    ✨ Add new main function (remove Fiber, add Echo)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,101 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/ispaneli/urlpresser/cmd/shortener/config"
+	"github.com/labstack/echo/v4"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
-	"strings"
-	"sync"
 
-	"github.com/valyala/fasthttp"
+	"github.com/ispaneli/urlpresser/internal/config"
+	"github.com/ispaneli/urlpresser/internal/handlers"
+	"github.com/ispaneli/urlpresser/internal/storage"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-type URLMap struct {
-	// ShortURLMap stores the mapping of shortened URLs to original URLs.
-	ShortURLMap map[string]string
-	// OriginalURLMap stores the mapping of original URLs to shortened URLs.
-	OriginalURLMap map[string]string
-
-	sync.Mutex
-}
-
-func generateShortURL() string {
-	/*
-	   Generating a random string.
-
-	   Description:
-	   Function to generate a unique identifier for the shortened link.
-	*/
-	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	result := make([]byte, 8)
-	for i := range result {
-		result[i] = chars[randomInt(0, len(chars)-1)]
-	}
-	return string(result)
-}
-
-func randomInt(min int, max int) int {
-	/*
-	   Generating a random number.
-	*/
-	return min + rand.Intn(max-min+1)
-}
-
-func shortingURLHandler(ctx *fasthttp.RequestCtx, baseURL string) {
-	/*
-	   Handler for URL shortening.
-	*/
-	originalURL := strings.TrimSpace(string(ctx.PostBody()))
-	if originalURL == "" {
-		ctx.SetStatusCode(http.StatusBadRequest)
-		ctx.SetBodyString("Invalid request")
-		return
-	}
-
-	// Check if the original URL already exists in OriginalURLMap.
-	existShortURL, ok := OriginalURLMap[originalURL]
-	if ok {
-		responseURL := fmt.Sprintf(`%s%s`, baseURL, existShortURL)
-		ctx.SetStatusCode(http.StatusCreated)
-		ctx.SetBodyString(responseURL)
-		return
-	}
-
-	// Generate a unique identifier for the shortened link.
-	for {
-		shortURL := generateShortURL()
-		if _, ok := ShortURLMap[shortURL]; !ok {
-			ShortURLMap[shortURL] = originalURL
-			OriginalURLMap[originalURL] = shortURL
-
-			responseURL := fmt.Sprintf(`%s%s`, baseURL, shortURL)
-			ctx.SetStatusCode(http.StatusCreated)
-			ctx.SetBodyString(responseURL)
-			return
-		}
-	}
-}
-
-func redirectingURLHandler(ctx *fasthttp.RequestCtx) {
-	/*
-	   Handler for redirecting to the original URL.
-	*/
-	shortURL := ctx.UserValue("id").(string)
-	originalURL, ok := ShortURLMap[shortURL]
-	if !ok {
-		ctx.SetStatusCode(http.StatusBadRequest)
-		ctx.SetBodyString("Invalid short URL")
-		return
-	}
-
-	ctx.Redirect(originalURL, http.StatusTemporaryRedirect)
-}
-
 func main() {
-	// Parsing command args.
 	cfg := config.InitConfig()
 	fmt.Printf("HTTP Address: %s\n", cfg.HTTPAddress)
 	fmt.Printf("Base URL: %s\n", cfg.BaseURL)
@@ -103,7 +22,6 @@ func main() {
 	HTTPAddress := cfg.HTTPAddress
 	baseURL := cfg.BaseURL
 
-	// Parsing environment variables.
 	if address := os.Getenv("SERVER_ADDRESS"); address != "" {
 		HTTPAddress = address
 	}
@@ -111,24 +29,16 @@ func main() {
 		baseURL = url
 	}
 
-	// Creating and configuring web-application.
-	server := &fasthttp.Server{
-		Handler: func(ctx *fasthttp.RequestCtx) {
-			switch string(ctx.Path()) {
-			case "/":
-				if ctx.IsPost() {
-					shortingURLHandler(ctx, baseURL)
-				} else {
-					ctx.SetStatusCode(http.StatusMethodNotAllowed)
-				}
-			default:
-				redirectingURLHandler(ctx)
-			}
-		},
-	}
+	s := storage.NewStorage()
 
-	err := server.ListenAndServe(HTTPAddress)
-	if err != nil {
+	h := handlers.NewHandlers(s, baseURL)
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.POST("/", h.ShortingURLHandler)
+	e.GET("/:id", h.RedirectingURLHandler)
+
+	if err := e.Start(HTTPAddress); !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
 }
