cb19c6a7856f03bd905b3d138b45047201c4086d
ispaneli <ilia.bezverzhenko@mail.ru>
2023-09-28 05:28:58 +0300

    ✨ Add storage path to config (increment №9)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,38 +2,22 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-
 	"github.com/ispaneli/urlpresser/internal/config"
 	"github.com/ispaneli/urlpresser/internal/handlers"
 	"github.com/ispaneli/urlpresser/internal/logger"
 	"github.com/ispaneli/urlpresser/internal/storage"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"net/http"
 )
 
 func main() {
 	cfg := config.InitConfig()
-	fmt.Printf("HTTP Address: %s\n", cfg.HTTPAddress)
-	fmt.Printf("Base URL: %s\n", cfg.BaseURL)
-
-	httpAddress := cfg.HTTPAddress
-	baseURL := cfg.BaseURL
-
-	if address := os.Getenv("SERVER_ADDRESS"); address != "" {
-		httpAddress = address
-	}
-	if url := os.Getenv("BASE_URL"); url != "" {
-		baseURL = url
-	}
 
 	s := storage.NewStorage()
 
-	h := handlers.NewHandlers(s, baseURL)
+	h := handlers.NewHandlers(s, cfg.BaseURL)
 
 	e := echo.New()
 	e.Use(middleware.RequestLoggerWithConfig(logger.HandlerLoggerConfig))
@@ -42,7 +26,7 @@ func main() {
 	e.GET("/:id", h.RedirectingURLHandler)
 	e.POST("/api/shorten", h.ShortenAPIHandler)
 
-	if err := e.Start(httpAddress); !errors.Is(err, http.ErrServerClosed) {
+	if err := e.Start(cfg.HTTPAddress); !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
 }
